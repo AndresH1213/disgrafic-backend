4232d90ba888e64b9c656df65c8be99870494996
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-15 23:32:46 -0500

    update single product handler function

---
+++ src/product/update.py
@@ -0,0 +1,41 @@
+import json
+import logging
+
+from pynamodb.exceptions import DoesNotExist
+from lib.models.Product import ProductModel
+
+
+def handler(event, context):
+    # TODO: Figure out why this is behaving differently to the other endpoints
+    # data = json.loads(event['body'])
+    data = event['body']
+
+    if 'text' not in data and 'checked' not in data:
+        logging.error('Validation Failed %s', data)
+        return {'statusCode': 422,
+                'body': json.dumps({'error_message': 'Couldn\'t update the product.'})}
+
+    try:
+        found_product = ProductModel.get(hash_key=event['path']['id'])
+    except DoesNotExist:
+        return {'statusCode': 404,
+                'body': json.dumps({'error_message': 'Product was not found'})}
+
+    product_changed = False
+    if 'name' in data and data['name'] != found_product.name:
+        found_product.name = data['name']
+        product_changed = True
+    if 'price' in data and data['price'] != found_product.price:
+        found_product.price = data['price']
+        product_changed = True
+    if 'label' in data and data['label'] != found_product.label:
+        found_product.label = data['label']
+        product_changed = True
+
+    if product_changed:
+        found_product.save()
+    else:
+        logging.info('Nothing changed did not update')
+
+    return {'statusCode': 200,
+            'body': json.dumps(dict(found_product))}
