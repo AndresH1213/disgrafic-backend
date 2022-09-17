ac71ff60229d71c14749fef9874f805d5e9a5f63
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-17 10:57:14 -0500

    headers and response methods

---
+++ lib/shared/headers.py
@@ -0,0 +1,6 @@
+def get_default_headers():
+    return {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+    }
