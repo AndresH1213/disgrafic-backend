187a74b2f909d3470f3babba7e8352fbafd33f54
Andres Hernandez <edanhebla1213@hotmail.com>
2022-10-12 23:49:11 -0500

    list products handlers

--- src/product/list.py
+++ src/product/list.py
@@ -3,9 +3,19 @@ import json
 from lib.models.Product import ProductModel
 from lib.shared.response import send_response
 
+ALLOWED_LABEL = ['material', 'mercancia']
+
 
 def handler(event, context):
-    results = ProductModel.scan()
+    q = 'queryStringParameters'
+    query_params = q in event and event[q]
+
+    if query_params and "label" in query_params and query_params['label'] in ALLOWED_LABEL:
+        results = list(ProductModel.label_index.query(
+            str(query_params['label'])))
+    else:
+        results = ProductModel.scan()
+
     products = {
         'products': [dict(result) for result in results]
     }
